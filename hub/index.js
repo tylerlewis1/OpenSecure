96daa7742a29b937b07c79d921b362e035ce7dbb
Tyler Lewis <tglewis247@gmail.com>
2026-02-27 20:02:24 -0600

    added status endpoint

--- hub/index.js
+++ hub/index.js
@@ -1,5 +1,19 @@
 const startBroker = require('./services/broker');
 const dotenv = require('dotenv');
+const express = require('express');
+const app = express();
+
 dotenv.config();
 
+app.listen(process.env.PORT, () => {
+  console.log(`Hub is on port ${process.env.PORT}`);
+});
+
+app.get("/status", (req, res) => {
+  res.send("OpenSecure Hub is running!");
+});
+
+
 startBroker();
+
+app.use(express.json());
